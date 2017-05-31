6e854310eced2cda4df87e5de06f6267d6a29356
Gonçalo Sá <goncalo05@gmail.com>
2017-05-31 18:41:36 +0100

    adding more instantiation tests

--- test/StandardBounty.js
+++ test/StandardBounty.js
@@ -2,16 +2,17 @@
 const StandardBounty = artifacts.require('./StandardBounty.sol');
 
 const fs = require('fs');
+const utils = require('./helpers/utils');
 
 let bounty;
 let bountyObj;
 
 async function generateBounty(bountyObjParam) {
-  return await StandardBounty.new(
+  return StandardBounty.new(
     bountyObjParam.deadline,
     bountyObjParam.issuerContact,
     bountyObjParam.data,
-    bountyObjParam.fullfilmentAmount
+    bountyObjParam.fulfillmentAmount
   );
 }
 
@@ -19,36 +20,131 @@ contract('StandardBounty', (accounts) => {
   let config = JSON.parse(fs.readFileSync('./test/conf/configObjects.json'));
   let [issuer, normalBountyHunter, maliciousBountyHunter] = accounts;
 
-  describe('Good Bounty Instantiation', () => {
+  describe('\nGood Bounty Instantiation', () => {
     before(async () => {
       bountyObj = config.exampleStandardBounties.good;
+      
+      console.log(`\nInstantiating good bounty with the following parameters: `);
       console.log(bountyObj);
+      console.log(`\t`);
+
       bounty = await generateBounty(bountyObj);
     });
 
     it(`should instantiate with ${issuer} as issuer.`, async () => {
-      const issuer = await bounty.issuer.call();
-      assert.equal(name, issuer);
+      let issuerCur = await bounty.issuer.call();
+      assert.equal(issuerCur, issuer);
     });
 
-    // it(`should instantiate with ${bountyObj.deadline} as deadline.`, async () => {
-    //   const dealine = await bounty.dealine.call();
-    //   assert.equal(name, bountyObj.deadline);
-    // });
+    it(`should instantiate with correct deadline.`, async () => {
+      let deadline = await bounty.deadline.call();
+      assert.equal(deadline, bountyObj.deadline);
+    });
 
-    // it(`should instantiate with ${bountyObj.issuerContact} as issuerContact.`, async () => {
-    //   const issuerContact = await bounty.issuerContact.call();
-    //   assert.equal(name, bountyObj.issuerContact);
-    // });
+    it(`should instantiate with correct issuerContact.`, async () => {
+      let issuerContact = await bounty.issuerContact.call();
+      assert.equal(issuerContact, bountyObj.issuerContact);
+    });
 
-    // it(`should instantiate with ${bountyObj.data} as data.`, async () => {
-    //   const issuer = await bounty.issuer.call();
-    //   assert.equal(name, bountyObj.data);
-    // });
+    it(`should instantiate with correct data.`, async () => {
+      let data = await bounty.data.call();
+      assert.equal(data, bountyObj.data);
+    });
 
-    // it(`should instantiate with ${bountyObj.fullfilmentAmount} as fullfilmentAmount.`, async () => {
-    //   const issuer = await bounty.issuer.call();
-    //   assert.equal(name, bountyObj.fullfilmentAmount);
-    // });
+    it(`should instantiate with correct fulfillmentAmount.`, async () => {
+      let fulfillmentAmount = await bounty.fulfillmentAmount.call();
+      assert.equal(fulfillmentAmount, bountyObj.fulfillmentAmount);
+    });
+  });
+
+  describe('\nBad 0-Data-Bounty Instantiation', () => {
+    before(async () => {
+      bountyObj = config.exampleStandardBounties.bad.zeroData;
+      
+      console.log(`\nInstantiating bad bounty with the following parameters: `);
+      console.log(bountyObj);
+      console.log(`\t`);
+
+      bounty = await generateBounty(bountyObj);
+    });
+
+    it(`should instantiate with ${issuer} as issuer.`, async () => {
+      let issuerCur = await bounty.issuer.call();
+      assert.equal(issuerCur, issuer);
+    });
+
+    it(`should instantiate with correct deadline.`, async () => {
+      let deadline = await bounty.deadline.call();
+      assert.equal(deadline, bountyObj.deadline);
+    });
+
+    it(`should instantiate with correct issuerContact.`, async () => {
+      let issuerContact = await bounty.issuerContact.call();
+      assert.equal(issuerContact, bountyObj.issuerContact);
+    });
+
+    it(`should instantiate with correct data.`, async () => {
+      let data = await bounty.data.call();
+      assert.equal(data, bountyObj.data);
+    });
+
+    it(`should instantiate with correct fulfillmentAmount.`, async () => {
+      let fulfillmentAmount = await bounty.fulfillmentAmount.call();
+      assert.equal(fulfillmentAmount, bountyObj.fulfillmentAmount);
+    });
+  });
+
+  describe('\nBad 0-Deadline-Bounty Instantiation', () => {
+    before(async () => {
+      bountyObj = config.exampleStandardBounties.bad.zeroDeadline;
+      
+      console.log(`\nInstantiating bad bounty with the following parameters: `);
+      console.log(bountyObj);
+      console.log(`\t`);
+
+      bounty = await generateBounty(bountyObj);
+    });
+
+    it(`should instantiate with ${issuer} as issuer.`, async () => {
+      let issuerCur = await bounty.issuer.call();
+      assert.equal(issuerCur, issuer);
+    });
+
+    it(`should instantiate with correct deadline.`, async () => {
+      let deadline = await bounty.deadline.call();
+      assert.equal(deadline, bountyObj.deadline);
+    });
+
+    it(`should instantiate with correct issuerContact.`, async () => {
+      let issuerContact = await bounty.issuerContact.call();
+      assert.equal(issuerContact, bountyObj.issuerContact);
+    });
+
+    it(`should instantiate with correct data.`, async () => {
+      let data = await bounty.data.call();
+      assert.equal(data, bountyObj.data);
+    });
+
+    it(`should instantiate with correct fulfillmentAmount.`, async () => {
+      let fulfillmentAmount = await bounty.fulfillmentAmount.call();
+      assert.equal(fulfillmentAmount, bountyObj.fulfillmentAmount);
+    });
+  });
+
+  describe('\nBad 0-Fulfillment-Amount-Bounty Instantiation', () => {
+    it(`should throw when instantiating with correct data.`, async () => {
+      bountyObj = config.exampleStandardBounties.bad.zeroFulfillmentAmount;
+
+      console.log(`\nInstantiating bad bounty with the following parameters: `);
+      console.log(bountyObj);
+      console.log(`\t`);
+
+      try {
+        bounty = await generateBounty(bountyObj);
+      }
+      catch (error) {
+        return utils.ensureException(error);
+      }
+    });
   });
 });
---
+++ test/helpers/utils.js
@@ -0,0 +1,17 @@
+/* global assert */
+/* Authored by the Bancor Protocol team */
+
+function isException(error) {
+    let strError = error.toString();
+    return strError.includes('invalid opcode') || strError.includes('invalid JUMP');
+}
+
+function ensureException(error) {
+    assert(isException(error), error.toString());
+}
+
+module.exports = {
+    zeroAddress: '0x0000000000000000000000000000000000000000',
+    isException: isException,
+    ensureException: ensureException
+};
